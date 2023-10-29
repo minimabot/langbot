c28cf05ac820f7b096bc49067a8a1b72ac339249
Myeonghwan Cho <myeonghcho@gmail.com>
2023-10-29 14:58:07 +0900

    Use SSL with custom domain for localhost

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,10 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  config.force_ssl = true
+
+  # Enable DNS rebinding protection and other `Host` header attacks.
+  config.hosts << '.langbot.localhost'
 end
